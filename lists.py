69328ad7b4aa577f9adf59e63b97e72d8b791e14
MateoBeumont <mbeumont@mail.austral.edu.ar>
2025-04-28 19:16:51 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -12,17 +12,10 @@ def add_elements(list_to_add_elements):
 	return y
 	
 def is_empty(list_to_check):
-	if list_to_check == []:
-		rta = "True"
-	else:
-		rta = "False"
-	return rta
-
+	return len(list_to_check) == 0
+		
 def check_lists(list_to_compare1, list_to_compare2):
-	if list_to_compare1[2] == list_to_compare2[2]:
-		rta = "True"
-	else: 
-		rta = "False"
+	return list_to_compare1[2] == list_to_compare2[2]:
 
 def list_of_lists(list_of_lists_to_modify):
 	x = list_of_lists_to_modify[0][0:2]
