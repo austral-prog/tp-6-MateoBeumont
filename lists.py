d2bb9f21215bb2eda74c1b0452a0b16f3c95aa1f
MateoBeumont <mbeumont@mail.austral.edu.ar>
2025-04-28 17:00:08 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,31 +1,31 @@
 # Replace the "ANSWER HERE" with your answer
 
 def remove_elements(list_to_remove_elements):
-    del list_to_remove_elements[0]
+	del list_to_remove_elements[0]
 	del list_to_remove_elements[4]
 	del list_to_remove_elements[5]
 	return list_to_remove_elements
 
 def add_elements(list_to_add_elements):
-    list_to_add_elements.insert(0, "pink")
+	list_to_add_elements.insert(0, "pink")
 	list_to_add_elements.append("yellow")
 	return list_to_add_elements
 
 def is_empty(list_to_check):
-    if list_to_check == []:
+	if list_to_check == []:
 		rta = "True"
 	else:
 		rta = "False"
 	return rta
 
 def check_lists(list_to_compare1, list_to_compare2):
-    if list_to_compare1[2] == list_to_compare2[2]:
+	if list_to_compare1[2] == list_to_compare2[2]:
 		rta = "True"
 	else: 
 		rta = "False"
 
 def list_of_lists(list_of_lists_to_modify):
-    list_of_lists_to_modify[0][0:2]
+	list_of_lists_to_modify[0][0:2]
 	list_of_lists_to_modify[1][1:4]
 	list_of_lists_to_modify[3][0:2:-1]
-    return f
+	return f
