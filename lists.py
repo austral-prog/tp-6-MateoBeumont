4823e7a0c555e026a837217a860c735a275f0c15
MateoBeumont <mbeumont@mail.austral.edu.ar>
2025-04-28 19:27:27 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -15,7 +15,9 @@ def is_empty(list_to_check):
 	return len(list_to_check) == 0
 		
 def check_lists(list_to_compare1, list_to_compare2):
-	return list_to_compare1[2] == list_to_compare2[2]:
+	if len(list_to_compare1) >= 3 and len(list_to_compare2) >= 3
+		return list_to_compare1[2] == list_to_compare2[2]:
+	return false
 
 def list_of_lists(list_of_lists_to_modify):
 	x = list_of_lists_to_modify[0][0:2]
