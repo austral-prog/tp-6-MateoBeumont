1b21bbdd6ff0d5d6c38e34b9ab72b67aaec17db0
MateoBeumont <mbeumont@mail.austral.edu.ar>
2025-05-05 22:03:32 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,22 +1,29 @@
 # Replace the "ANSWER HERE" with your answer
-def remove_elements(list_to_remove_elements):
-	del list_to_remove_elements[5]
-	del list_to_remove_elements[4]
-	del list_to_remove_elements[0]
-	return list_to_remove_elements
+def remove_elements(lista):
+    for i in sorted([5, 4, 0], reverse=True):
+        if i < len(lista):
+         del lista[i]
+    return lista
 
 def add_elements(list_to_add_elements):
-	list_to_add_elements.insert(0, "Pink")
-	list_to_add_elements.append("Yellow")
-	return list_to_add_elements
+    list_to_add_elements.insert(0,"Pink")
+    list_to_add_elements.append("Yellow")
+    return list_to_add_elements
 
 def is_empty(list_to_check):
-	return len(list_to_check) == 0
-		
+    if len(list_to_check) == 0:
+        return True
+    else:
+        return False
+
 def check_lists(list_to_compare1, list_to_compare2):
-	if len(list_to_compare1) >= 3 and len(list_to_compare2) >= 3
-		return list_to_compare1[2] == list_to_compare2[2]:
-	return false
+    if len(list_to_compare1)<3 or len(list_to_compare2)<3:
+        return False
+    else :
+        if list_to_compare1[2] == list_to_compare2[2]:
+            return True
+        else:
+            return False
 
 def list_of_lists(list_of_lists_to_modify):
     if len(list_of_lists_to_modify[0]) < 3:
